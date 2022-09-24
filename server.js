c4a30e0e6277fafb766932a2838a134f26fc3ec2
Talha Imran <talhaimran284@gmail.com>
2022-09-24 23:15:45 +0500

    Added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import { createServer } from "http";
 import { Server } from "socket.io";
@@ -6,6 +7,8 @@ import { Server } from "socket.io";
 dotenv.config();
 
 const app = express();
+app.use(cors);
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
